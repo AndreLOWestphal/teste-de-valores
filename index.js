add74c568b746801800bb2606b22a756d5f55b61
André Luís <125711609+AndreLOWestphal@users.noreply.github.com>
2023-08-21 21:55:01 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let productValue = Number(prompt('Enter the product value: '))
+
+    if (productValue >= 20 ){
+        document.getElementById('result').innerHTML = 'Approved'
+    }
+    else { document.getElementById('result').innerHTML = 'Denied'
+    }
